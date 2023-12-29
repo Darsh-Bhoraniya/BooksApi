d994ef0d48d9f611bc93b77b9f82d9f22efd1877
Darsh bhoraniya <darshpatel269@gmail.com>
2023-12-29 16:42:15 +0530

    Initail Commit

--- BooksApi/Model/User_Model.cs
+++ BooksApi/Model/User_Model.cs
@@ -9,6 +9,6 @@
         public string Password { get; set; }
         public int RoleID { get; set; }
         public DateTime Created { get; set; }
-        public DateTime Modified { get; set; }
+        public DateTime Modified { get; set; }  
     }
 }
