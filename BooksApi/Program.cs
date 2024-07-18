97e5e65a55d3aa65f00efea3f020749621adb5dc
Darsh-Bhoraniya <darshpatel269@gmail.com>
2024-07-18 16:30:58 +0530

    Add A Cloud DataBase on a Server

--- BooksApi/Program.cs
+++ BooksApi/Program.cs
@@ -1,11 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin() // Replace with your allowed origin
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -17,6 +29,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowSpecificOrigins"); // Use the CORS policy
 
 app.UseAuthorization();
 
