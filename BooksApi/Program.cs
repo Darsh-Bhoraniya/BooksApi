4c76855aaec221cf7830377122510976aa4a1078
Darsh-Bhoraniya <darshpatel269@gmail.com>
2024-07-18 17:29:38 +0530

    Cloude DataBase Updated

--- BooksApi/Program.cs
+++ BooksApi/Program.cs
@@ -1,36 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
 
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Add CORS policy
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigins",
-        builder =>
-        {
-            builder.AllowAnyOrigin() // Replace with your allowed origin
-                   .AllowAnyHeader()
-                   .AllowAnyMethod();
-        });
-});
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+    
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-
-app.UseCors("AllowSpecificOrigins"); // Use the CORS policy
 
 app.UseAuthorization();
 
