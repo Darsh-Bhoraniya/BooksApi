f707fdb35d2a2a394ae65868d82a269ca890a63d
Darsh-Bhoraniya <darshpatel269@gmail.com>
2024-10-01 18:42:05 +0530

    Tested For OTP

--- BooksApi/DAL/DAL_Helper.cs
+++ BooksApi/DAL/DAL_Helper.cs
@@ -3,7 +3,7 @@
     public class DAL_Helper
     {
 
-        public static string ConnString = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("MyConnectionString");
+        public static string ConnString = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("DefaultConnection");
 
     }
 }
